12797aa2708fe92ef78771be85637ef464127fae
pingudiogo <digax96@gmail.com>
2016-10-16 22:40:09 +0100

    Display Board Done

--- Project 1/src/board.pl
+++ Project 1/src/board.pl
@@ -6,8 +6,6 @@ piece_empty([
     [' ',' ',' ',' ',' ']
     ]).
 
-piece_empty(E).
-
 white_small([
     [' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' '],
@@ -32,44 +30,84 @@ white_big([
 red_small([
     [' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' '],
-    [' ',' ','W',' ',' '],
+    [' ',' ','R',' ',' '],
     [' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' ']]).
 
 red_medium([
     [' ',' ',' ',' ',' '],
-    [' ','W','W','W',' '],
-    [' ','W','W','W',' '],
-    [' ','W','W','W',' '],
+    [' ','R','R','R',' '],
+    [' ','R','R','R',' '],
+    [' ','R','R','R',' '],
     [' ',' ',' ',' ',' ']]).
 
 red_big([
-    ['W','W','W','W','W'],
-    ['W','W','W','W','W'],
-    ['W','W','W','W','W'],
-    ['W','W','W','W','W'],
-    ['W','W','W','W','W']]).
-
-empty_board([
-    [E,E,E,E,E,E,E,E,E],
-    [E,E,E,E,E,E,E,E,E],
-    [E,E,E,E,E,E,E,E,E],
-    [E,E,E,E,E,E,E,E,E],
-    [E,E,E,E,E,E,E,E,E],
-    [E,E,E,E,E,E,E,E,E],
-    [E,E,E,E,E,E,E,E,E],
-    [E,E,E,E,E,E,E,E,E],
-    [E,E,E,E,E,E,E,E,E]
-    ]).
+    ['R','R','R','R','R'],
+    ['R','R','R','R','R'],
+    ['R','R','R','R','R'],
+    ['R','R','R','R','R'],
+    ['R','R','R','R','R']]).
 
 
+%Create Board
 
-display_board([H|T]):- display_line(73), display_board_row(H,1), display_board(T).
+create_board(H):- 
+            length(H,9),
+            create_rows(H).
 
-display_board_row([H|T],N):- N<4, display_board_line(H,N), write('|\n'),N1 is N+1,display_board_row(T,N1).
+create_rows([]).
+create_rows([H|T]):- 
+            length(H,9),
+            create_pieces(H),
+            create_rows(T).
 
-display_board_line([H|T],N):- write('| '), nth1(N,H,R), display_piece_line(R), write(' '), display_board_line(T,N).
+create_pieces([]).
+create_pieces([H|T]):- 
+            piece_empty(H),
+            create_pieces(T).
 
-display_piece_line([H|T]):- write(H), display_piece_line(T).
+%Display Board
 
-display_line(N):- N>1, N1 is N-1, write('-'), display_line(N1).
+display_board(H):- 
+            display_line(73),
+            display_line(73), 
+            display_board_aux(H).
+
+displat_board_aux([]).
+display_board_aux([H|T]):- 
+            display_board_row(H,1), 
+            display_board_aux(T).
+
+display_board_row([],N).
+display_board_row(H,N):-
+            N>3,
+            display_line(73).
+display_board_row(H,N):- 
+            N < 4,
+            display_board_line(H,N), 
+            write('|'),
+            nl,
+            N1 is N+1,
+            display_board_row(H,N1).
+
+display_board_line([],N).
+display_board_line([H|T],N):- 
+            write('| '), 
+            nth1(N,H,R), 
+            display_piece_line(R), 
+            write(' '), 
+            display_board_line(T,N).
+
+display_piece_line([]).
+display_piece_line([H|T]):- 
+            write(H), 
+            display_piece_line(T).
+
+display_line(N):-
+            N<1,
+            nl.
+display_line(N):-
+            N>0,
+            N1 is N-1, 
+            write('-'), 
+            display_line(N1).
