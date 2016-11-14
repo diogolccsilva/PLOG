fb3ddacf65b2a306ea654e1ef6b3ebb6262fa195
Diogo Silva <digax96@gmail.com>
2016-11-14 01:00:38 +0000

    I AM HOKAGE

--- Project 1/src/oshi.pl
+++ Project 1/src/oshi.pl
@@ -7,9 +7,8 @@ oshi :-
 chooseGameMode :-
 	write('There are 4 game modes you can choose:\n'),
 	write('1. Player vs Player;\n'),
-	write('2. Player vs AI (easy); (WIP)\n'),
-	write('3. Player vs AI (hard); (WIP)\n'),
-	write('4. AI vs AI; (WIP)\n'),
+	write('2. Player vs AI (easy);\n'),
+	write('3. AI vs AI;\n'),
 	write('Please choose one of the game modes listed above: '),
 	read(GM),
 	nl,
@@ -17,8 +16,9 @@ chooseGameMode :-
 
 startGame(GM):-
 	GM >= 1,
-	GM =< 4,
+	GM =< 3,
 	default_board(B),
+	!,
 	game(GM,B,0).
 startGame(GM):-
 	write('Option '),
@@ -27,41 +27,39 @@ startGame(GM):-
 	chooseGameMode.
 
 game(GM,B,TURN):-
-	GM = 1,
 	PL is mod(TURN,2),
 	display_board(B),
 	game_status(B),
+	check_game(B),
 	game_turn(PL),
 	repeat,
-	askForPiece(B,PR,PC,P,PL),
-	askForMove(D,N,P),
+	turn(GM,B,PL,PR,PC,D,N,P),
 	check_move(B,PR,PC,N,D),
-	move(B,PR,PC,N,D,NB),
 	!,
-	check_game(NB),
+	move(B,PR,PC,N,D,NB),
 	NT is TURN + 1,
 	game(GM,NB,NT).
-game(GM,B,TURN):-
-	GM = 2,
-	PL is mod(TURN,2),
-	display_board(B),
-	game_status(B),
-	game_turn(PL),
-	repeat,
-	randomPiece(B,PR,PC,P,PL),
-	randomMove(D,N,P),
-	check_move(B,PR,PC,N,D),
-	move(B,PR,PC,N,D,NB),
-	!,
-	check_game(NB),
-	NT is TURN + 1,
-	game(GM,NB,NT).
-game(GM,B,TURN):-
-	GM >= 3,
-	GM =< 4,
-	write('WORK IN PROGRESS\n').
 game(_,B,_):-
 	write('End of game!\n').
+
+turn(GM,B,PL,PR,PC,D,N,P):-
+	GM = 1,
+	askForPiece(B,PR,PC,P,PL),
+	askForMove(D,N,P).
+turn(GM,B,PL,PR,PC,D,N,P):-
+	GM = 2,
+	PL = 0,
+	askForPiece(B,PR,PC,P,PL),
+	askForMove(D,N,P).
+turn(GM,B,PL,PR,PC,D,N,P):-
+	GM = 2,
+	PL = 1,
+	randomPiece(B,PR,PC,P,PL),
+	randomMove(D,N,P).
+turn(GM,B,PL,PR,PC,D,N,P):-
+	GM = 3,
+	randomPiece(B,PR,PC,P,PL),
+	randomMove(D,N,P).
 
 game_status(B):-
 	count_points_white(B,W),
@@ -80,16 +78,23 @@ game_turn(PL):-
 	write('White\'s turn\n').
 
 check_game(B):-
+	check_white(B),
+	check_red(B).
+
+check_white(B):-
 	count_points_red(B,C),
-	C < 7,
+	C >= 7.
+check_white(_):-
 	write('White wins, congrats!\n'),
 	fail.
-check_game(B):-
+
+check_red(B):-
 	count_points_white(B,C),
-	C < 7,
+	C >= 7.
+check_red(_):-
 	write('Red wins, congrats!\n'),
 	fail.
-check_game(B).
+
 
 askForPiece(B,PR,PC,P,PL):-
 	repeat,
