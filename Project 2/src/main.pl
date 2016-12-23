5f92daf2cb1f104e2c850188561c9a3cafcc2cc0
Diogo Luís Silva <digax96@gmail.com>
2016-12-23 04:39:58 +0000

    Added file to be consulted

--- Project 2/src/main.pl
+++ Project 2/src/main.pl
@@ -1,4 +1,4 @@
 /* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:3; tab-width:8; -*- */
 
 
-:- consult(board),consult(test),consult(display),consult(fourwinds).
+:- consult(board),consult(test),consult(display),consult(fourwinds),consult(solver).
